85517ccf3681f2469a9733604331162043c84372
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-09-26 13:58:54 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ SEC_GROUP="sg-03c6dffbde757a30b"
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SEC_GROUP --tag-specifications ResourceType=instance,Tags=[{Key=Name,Value=$instance}] --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SEC_GROUP --tag-specifications ResourceType=instance,Tags=[{Key=Name,Value={$instance}}] --query 'Instances[0].InstanceId' --output text)
     
 if  [ $instance != "frontend" ]; then 
     IP=$( aws ec2 describe-instances --instance-ids  $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
