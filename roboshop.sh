21585ac99e4b9d27f23e410d038d8823d8c3cdc6
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-09-26 15:19:59 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+AIM_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-03c6dffbde757a30b"
 
 
-for i in {1..20}
+for instance in $@
 do
-    echo "number: $i"
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AIM_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+
+    if [ $instance != "frontend"]; then
+    
+        IP=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-07c8acf3fa6b923fa --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test}]' --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        else
+            IP=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-07c8acf3fa6b923fa --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test}]' --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+            fi
+            echo "$instance: $IP"
+
+
+
 done
     
