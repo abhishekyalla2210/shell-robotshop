28470c69f7884e64e6ae3c036b73fae66c162789
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-09-26 16:44:42 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -5,34 +5,31 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 W="\e[34m"
+N="\e[0m"
 
 LOGS="/var/log/shell-robotshop"
-SCRIPTNAME=$( echo $0 | cut -d "." -f1 )
-LOGFILE="$LOGS/$SCRIPTNAME.log" 
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE="$LOGS/$SCRIPTNAME.log"
 
-mkdir   -p  $LOGS
-echo "script start at: $(date)"
-
+mkdir -p $LOGS
+echo "script start at: $(date)" &>>$LOGFILE
 
 if [ $USERID -ne 0 ]; then
     echo -e " $R please login with root access $N"
     exit 1
 fi
 
-VALIDATE(){ # functions receive inputs through args just like shell script args
+VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e " $2 ... $R FAILURE $N" 
+        echo -e " $2 ... $R FAILURE $N"
         exit 1
     else
         echo -e " $2 ... $G SUCCESS $N"
     fi
 }
 
-
-
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "adding the repo"
 
-dnf install mongodb-org -y  &>>$LOGFILE
+dnf install mongodb-org -y &>>$LOGFILE
 VALIDATE $? "mongodb installation"
-
