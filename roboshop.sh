f3ff05464717a2c036ec78c34636b58dd7a410ba
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-09-26 16:06:11 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -28,7 +28,7 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
     fi
 }
 
-cp mongo.repo /etc/yum.reps.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "adding the repo"
 
 dnf install mongodb-org -y&>>$LOGFILE
