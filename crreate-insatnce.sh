3fd6f76355bc192b2544411a7495e11a44dd2034
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-09-27 12:17:56 +0530

    roboshop

--- crreate-insatnce.sh
+++ crreate-insatnce.sh
@@ -6,13 +6,13 @@ SG_ID="sg-03c6dffbde757a30b"
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=mongodb}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$( aws ec2 run-instances --image-id $AMI --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=mongodb}]" --query 'Instances[0].InstanceId' --output text)
 
 
 if [$instance != "frontend"]; then
-    IP=$(aws ec describe-instances --instance-ids $INSTANCE_ID --query "Reservation[0].Instances[0].PrivateIpAddress" --output text )
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservation[0].Instances[0].PrivateIpAddress" --output text )
 else
-     IP=$(aws ec describe-instances --instance-ids $INSTANCE_ID --query "Reservation[0].Instances[0].PublicIpAddress" --output text )
+     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservation[0].Instances[0].PublicIpAddress" --output text )
 
 fi
     echo "$instance: $IP"
