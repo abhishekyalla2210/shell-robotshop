16d9da7bedea91f5dee1f2506e5f021fa886980c
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-09-27 11:47:51 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -39,3 +39,9 @@ VALIDATE $? "enabled mongodb"
 
 systemctl start mongod
 VALIDATE $? "started mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "allowing all"
+
+systemctl restart mongod
+VALIDATE $? "restarted"
