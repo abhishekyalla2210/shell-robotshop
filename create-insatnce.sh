72629e6b6d505550d81ef47737861bf148c07b2a
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-09-27 12:46:46 +0530

    roboshop

--- create-insatnce.sh
+++ create-insatnce.sh
@@ -2,31 +2,38 @@
 
 AMI="ami-09c813fb71547fc4f"
 SG_ID="sg-03c6dffbde757a30b"
+OUTPUT_FILE="instances-info.txt"
 
-for instance in "$@"
-do
+# Clear old file if it exists
+mkdir -p $OUTPUT_FILE
+
+for instance in "$@";
+ do
+   
+
     INSTANCE_ID=$(aws ec2 run-instances \
-        --image-id $AMI \
+        --image-id "$AMI" \
         --instance-type t3.micro \
-        --security-group-ids $SG_ID \
+        --security-group-ids "$SG_ID" \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
         --query 'Instances[0].InstanceId' \
         --output text)
 
-    # Wait a few seconds to allow AWS to register the instance
-    sleep 5
+    sleep 10
 
     if [ "$instance" != "frontend" ]; then
         IP=$(aws ec2 describe-instances \
-            --instance-ids $INSTANCE_ID \
+            --instance-ids "$INSTANCE_ID" \
             --query "Reservations[0].Instances[0].PrivateIpAddress" \
             --output text)
     else
         IP=$(aws ec2 describe-instances \
-            --instance-ids $INSTANCE_ID \
+            --instance-ids "$INSTANCE_ID" \
             --query "Reservations[0].Instances[0].PublicIpAddress" \
             --output text)
     fi
 
-    echo "$instance: $IP"
+    echo "$instance: $IP (Instance ID: $INSTANCE_ID)" | tee -a "$OUTPUT_FILE"
 done
+
+echo " All instance info saved to $OUTPUT_FILE"
