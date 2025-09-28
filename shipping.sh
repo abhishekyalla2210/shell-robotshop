2c4ca0bce230447729542042cfda2828b19b626c
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-09-28 19:09:49 +0530

    roboshop

--- shipping.sh
+++ shipping.sh
@@ -11,6 +11,7 @@ LOGS="/var/log/shell-robotshop"
 SCRIPTNAME=$(echo $0 | cut -d "." -f1)
 LOGFILE="$LOGS/$SCRIPTNAME.log"
 MYSQL_IP=mysql.abhishekdev.fun
+SCRIPT_DIR=$(pwd)
 
 mkdir -p $LOGS
 echo "script start at: $(date)" &>>$LOGFILE
@@ -30,32 +31,33 @@ VALIDATE(){
 }
 dnf install maven -y
 VALIDATE $? "installed"
+
 useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
 VALIDATE $? "added system user"
+
 mkdir /app 
 VALIDATE $? "made directory"
+
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip 
 VALIDATE $? "downloaded"
+
 cd /app 
 unzip /tmp/shipping.zip
 VALIDATE $? "unzipped"
 cd /app 
 mvn clean package 
 VALIDATE $? "cleaned"
-cp shipping.service /etc/systemd/system/shipping.service
+
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
 VALIDATE "copied shipping service" 
+
 systemctl daemon-reload
 VALIDATE $? "reloaded"
+
 systemctl enable shipping
 VALIDATE $? "enabled" 
+
 systemctl start shipping
 VALIDATE $? "shipping started"
-dnf install mysql -y 
-VALIDATE $? "installed mysql"
-mysql -h $MYSQL_IP -uroot -pRoboShop@1 < /app/db/schema.sql
 
-mysql -h  $MYSQL_IP -uroot -pRoboShop@1 < /app/db/app-user.sql 
-mysql -h $MYSQL_IP -uroot -pRoboShop@1 < /app/db/master-data.sql
 
-systemctl restart shipping
-VALIDATE $? "restarted"
